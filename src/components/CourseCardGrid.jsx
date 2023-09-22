24c35932749df1c84366ea4e95e7f7c9ba676b22
maseh90 <MasonHartweger2023@u.northwestern.edu>
2023-09-22 01:43:36 -0500

    added functionality to select courses from the grid, users can select or deselect courses to add or remove from a state variable keeping track of selected courses

--- src/components/CourseCardGrid.jsx
+++ src/components/CourseCardGrid.jsx
@@ -1,10 +1,10 @@
 import CourseCard from './CourseCard';
 import './CourseCardGrid.css';
 
-const CourseCardGrid = ({courses}) => (
+const CourseCardGrid = ({courses,selectedCourses,toggleSelectedCourse}) => (
   <div className="course-card-grid">
       { courses.map(([id,course]) => 
-      <div key={id}><CourseCard course={course} /></div>)
+      <div key={id}><CourseCard course={course} selectedCourses={selectedCourses} toggleSelectedCourse={toggleSelectedCourse} /></div>)
     }
   </div>
 );
--- src/components/TermSelectorPage.jsx
+++ src/components/TermSelectorPage.jsx
@@ -8,17 +8,25 @@ const terms = {
   Spring: ''
 };
 
-const TermMenu = ({data}) => (
-    <CourseCardGrid courses={data}/>
+const TermMenu = ({data, selectedCourses, toggleSelectedCourse, selection}) => (
+    <CourseCardGrid courses={data} selectedCourses={selectedCourses} toggleSelectedCourse={toggleSelectedCourse} selection={selection}/>
 );
 
 const TermSelectorPage = ({data}) => {
   const [selection, setSelection] = useState(() => Object.keys(terms)[0]);
+  const [selectedCourses, setSelectedCourses] = useState([]);
   const reduced_data = Object.entries(data).filter((course) => course[1].term === selection);
+
+  const toggleSelectedCourse = (item) => setSelectedCourses(
+    selectedCourses.includes(item)
+    ? selectedCourses.filter(x => x !== item)
+    : [...selectedCourses, item]
+  );
+  console.log(selectedCourses);
   return (
     <div>
       <TermSelector terms={terms} selection={selection} setSelection={setSelection} />
-      <TermMenu selection={selection} data={reduced_data}/>
+      <TermMenu selectedCourses={selectedCourses} toggleSelectedCourse={toggleSelectedCourse} selection={selection} data={reduced_data}/>
     </div>
   );
 }
