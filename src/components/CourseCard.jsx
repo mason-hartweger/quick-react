8422c1227904e7c78af481514946d70832890283
maseh90 <MasonHartweger2023@u.northwestern.edu>
2023-09-29 15:40:50 -0500

    added admin user privileges as feature - only admins have the ability to edit the courses in the database

--- src/components/CourseCard.jsx
+++ src/components/CourseCard.jsx
@@ -5,26 +5,36 @@ import Modal from './Modal'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import { useAuthState } from "../utilities/firebase";
+import { useProfile } from "../utilities/profile";
 
 
 
 const CourseCard = ({course, selectedCourses, toggleSelectedCourse, unavailibleCourses}) => {
+
     const [openForm, setOpenForm] = useState(false);
 
     const openModalForm = () => setOpenForm(true);
     const closeModalForm = () => setOpenForm(false);
 
     const [user] = useAuthState();
+    const [profile, profileLoading, profileError] = useProfile();
+    const isAdmin = profile.isAdmin;
 
     let isLoggedIn = false;
     if (user != null) {
         isLoggedIn = true;
     };
 
+    for (let i = 0; i < selectedCourses.length;i++) {
+        if (selectedCourses[i].number === course.number && openForm) {
+                selectedCourses.splice(i,1);
+        }
+    }
+
     return (
   <div className="card m-1 p-2" key='{course.title}' onClick={() => !unavailibleCourses.includes(course) ? toggleSelectedCourse(course): ""}>
     <div className={`card-body ${selectedCourses.includes(course) ? 'selected' : ''}`}>
-        {isLoggedIn ?
+        {(isLoggedIn && isAdmin) ?
             (<button onClick={openModalForm} style={{float: 'right'}}>
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-pencil-square" viewBox="0 0 16 16">
                     <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
