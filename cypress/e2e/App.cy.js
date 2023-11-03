33c659430b856b8dcaf75e46ffd9a8627bd94bb9
maseh90 <MasonHartweger2023@u.northwestern.edu>
2023-11-02 22:28:03 -0500

    installed eemulators, setup for local testing, and confirmed deployed app still uses production database

--- cypress/e2e/App.cy.js
+++ cypress/e2e/App.cy.js
@@ -3,16 +3,16 @@
 describe ('Test App', () => {
 
   it ('launches', () => {
-    cy.visit ('/');
+    cy.visit('/');
   });
 
   it ('opens with Fall CS courses', () => {
-    cy.visit ('/');
+    cy.visit('/');
     cy.get('[data-cy=course]').should('contain', 'Fall CS');
   });
 
   it('shows Winter courses when Winter is selected', () => {
-    cy.visit ('/');
+    cy.visit('/');
     cy.get('[data-cy=Winter]').click();
     cy.get('[data-cy=course]').should('contain' ,'Winter');
   });
